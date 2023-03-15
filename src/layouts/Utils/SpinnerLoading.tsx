9bd5cb26cb1262ae078ff57b158632e15a19d7b7
mcvollenweider <mcvollenweider@gmail.com>
2023-03-15 18:01:15 -0400

    added util folder and spinner component for async api calls

---
+++ src/layouts/Utils/SpinnerLoading.tsx
@@ -0,0 +1,14 @@
+export const SpinnerLoading = () => {
+  //spinner loading icon for carousel
+
+  return (
+    <div
+      className="container m-5 d-flex justify-content-center"
+      style={{ height: 550 }}
+    >
+      <div className="spinner-border text-primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
