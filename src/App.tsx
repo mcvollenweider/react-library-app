d10ea80006515b488631f43cd8844529e6711b61
mcvollenweider <mcvollenweider@gmail.com>
2023-03-21 14:19:22 -0400

    installed react-router-dom updated App and Browser Router in index

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import { Homepage } from "./layouts/HomePage/Homepage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 
-
 export const App = () => {
   return (
     <div>
-   <Navbar/>
-   {/*<Homepage/>*/}
-   <SearchBooksPage/>
-    <Footer/>
+      <Navbar />
+      <Switch>
+        {/*exact overrides*/}
+        <Route path="/" exact>
+          <Redirect to='/home'/>
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+        <Route path="/search">
+          <SearchBooksPage />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
   );
-}
-
+};
