286ff5267e9b270e220d8ca55cb5027d9acd3bd3
mcvollenweider <mcvollenweider@gmail.com>
2023-03-22 10:54:18 -0400

    added useEffect to BookCheckoutPage

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,17 +1,66 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const BookCheckoutPage = () => {
-  
   const [book, setBook] = useState<BookModel>();
-  const [isLoadingBook, setIsLoadingBook] = useState(true);
-  const [httpError, setHtttpError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
-  {/*bookId is grabbing url ex: localhost:3000/checkout/<bookId> */}
-  const bookId = (window.location.pathname).split('/')[2];
+  /*bookId is grabbing url ex: localhost:3000/checkout/<bookId> */
 
+  const bookId = window.location.pathname.split("/")[2];
+
+  //this useEffect is only grabbing one book
+  useEffect(() => {
+    const fetchBook = async () => {
+      //create api to fetch books from spring book application
+      const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+      const response = await fetch(baseUrl);
+
+      if (!response.ok) {
+        //check to make sure we got data looking for
+        throw new Error("Something went wrong!");
+      }
+      //converts into json
+      const responseJson = await response.json();
+
+      const loadedBook: BookModel = {
+        id: responseJson.id,
+        title: responseJson.title,
+        author: responseJson.author,
+        description: responseJson.description,
+        copies: responseJson.copies,
+        copiesAvailable: responseJson.copiesAvailable,
+        category: responseJson.category,
+        img: responseJson.img,
+      };
+
+      setBook(loadedBook);
+      setIsLoading(false);
+    };
+    fetchBook().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
     return (
-  <div>
-<h3>Hi World!</h3>
-  </div>);
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h3>Hi World!</h3>
+    </div>
+  );
 };
