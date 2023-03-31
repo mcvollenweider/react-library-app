ad60f0c089143d2a76b96c7bca3110d516ddc76c
mcvollenweider <mcvollenweider@gmail.com>
2023-03-31 19:02:02 -0400

    added OktaAuth, toRelativeUrl, oktaConfig to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import "./App.css";
 import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
 import { Homepage } from "./layouts/HomePage/Homepage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { oktaConfig } from "./lib/oktaConfig";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+  const customAuthHandler = () => {
+    history.push("/login");
+  };
+
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || "/", window.location.origin));
+  };
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <div className="flex-grow-1"> {/*this div is for the footer to stay on bottom */}
-      <Switch>
-        {/*exact overrides*/}
-        <Route path="/" exact>
-          <Redirect to='/home'/>
-        </Route>
-        <Route path="/home">
-          <Homepage /> 
-        </Route>
-        <Route path="/search">
-          <SearchBooksPage />
-        </Route>
-        <Route path="/checkout/:bookId">
-          <BookCheckoutPage />
-        </Route>
-      </Switch>
+      <div className="flex-grow-1">
+        {" "}
+        {/*this div is for the footer to stay on bottom */}
+        <Switch>
+          {/*exact overrides*/}
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <Homepage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+          <Route path="/checkout/:bookId">
+            <BookCheckoutPage />
+          </Route>
+        </Switch>
       </div>
       <Footer />
     </div>
